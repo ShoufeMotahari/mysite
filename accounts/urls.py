b4f73e2f5839b593611f63d70736adbeb58ae340
motSh79 <shokofe046@gmail.com>
2025-07-02 16:23:37 +0330

    General(main) fix: add core urls path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import signup_view, verify_view
+from .views import signup_view, verify_view, login_view, verify_login_view, dashboard_view
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('verify/', verify_view, name='verify'),
+    path('login/', login_view, name='login'),
+    path('verify-login/', verify_login_view, name='verify-login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
