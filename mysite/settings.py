95f36c991e80f45ecea13929cdef9b9f03d37a14
motSh79 <shokofe046@gmail.com>
2025-07-30 16:23:09 +0330

    genera  (main) fix: fox allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
