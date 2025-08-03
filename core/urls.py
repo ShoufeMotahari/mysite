ca0501a7b4803870384c5df85a639c27ab857323
motSh79 <shokofe046@gmail.com>
2025-08-03 10:41:15 +0330

    email (main) fix: fix email test

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home_view
-
+from core.views import test_email_view
 urlpatterns = [
     path('', home_view, name='home'),
+    path('test-email/', test_email_view, name='test-email'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,29 @@ import logging
 logger = logging.getLogger('core')
 def home_view(request):
     return HttpResponse("<h1>من بالاخره اجرا شدم :)✅</h1>")
+
+# core/views.py
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
+
+def test_email_view(request):
+    try:
+        logger.warning(f"📧 Trying to send email from: {settings.DEFAULT_FROM_EMAIL}")
+
+        send_mail(
+            subject='تست ایمیل از هاست',
+            message='این ایمیل تستی از ویوی تستی هست.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=['your_email@example.com'],  # آدرس ایمیل خودت رو بذار اینجا
+            fail_silently=False,
+        )
+
+        return HttpResponse("✅ ایمیل با موفقیت ارسال شد.")
+    except Exception as e:
+        logger.error(f"❌ خطا در ارسال ایمیل: {e}")
+        return HttpResponse(f"❌ خطا در ارسال ایمیل: {e}")
+
