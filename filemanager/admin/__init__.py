012650a4df1fa2b7a307a9fff5a503622ff4a6f8
motSh79 <shokofe046@gmail.com>
2025-08-20 17:16:48 +0330

    design pattern  (main) fix: fix  __init__.py filemanager admin and users admin

---
+++ filemanager/admin/__init__.py
@@ -0,0 +1,3 @@
+from .register.document_admin import *
+from .register.image_gallery_admin import *
+from .register.image_upload_admin import *
---
+++ users/admin/__init__.py
@@ -0,0 +1,14 @@
+# users/admin/__init__.py
+
+from .user.user_admin import *
+from .user.user_type_admin import *
+from .comment_admin import *
+from .email_template_admin import *
+
+from .admin_message.admin_message_admin import *
+from .admin_message.admin_message_reply_admin import *
+
+from .filter.user_type_filter import *
+from .filter.email_status_filter import *
+
+from .in_line.in_ilne import *
