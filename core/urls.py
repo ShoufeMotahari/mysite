9608a315496db824745f65ea4a411e5421800200
motSh79 <shokofe046@gmail.com>
2025-08-20 16:15:20 +0330

    design pattern  (main) fix: fix deleted fun

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from .views import (
     test_email_view,
     test_email_configuration_view,
     send_bulk_email_view,
-    email_service_test_view,
     email_stats_view
 )
 
@@ -18,7 +17,6 @@ urlpatterns = [
     # Email testing views
     path("test-email/", test_email_view, name="test-email"),
     path("test-email-config/", test_email_configuration_view, name="test-email-config"),
-    path("email-service-test/", email_service_test_view, name="email-service-test"),
 
     path("send-bulk-email/", send_bulk_email_view, name="send-bulk-email"),
     path("email-stats/", email_stats_view, name="email-stats"),
